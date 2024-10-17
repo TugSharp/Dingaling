b0ff352c39deedf41f7c7bcb041704392416ed35
TugSharp <smailbegovic.faris@gmail.com>
2024-10-17 20:30:17 +0200

    Fixed scaling, movement, Cheech sprite

--- nodes/character_body_2d.gd
+++ nodes/character_body_2d.gd
@@ -1,25 +1,11 @@
 extends CharacterBody2D
 
+@export var speed = 50
 
-const SPEED = 500.0
-const JUMP_VELOCITY = -400.0
-
+func get_input():
+	var input_direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity = input_direction * speed
 
 func _physics_process(delta: float) -> void:
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var directionX := Input.get_axis("ui_left", "ui_right")
-	if directionX:
-		velocity.x = directionX * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	# UP and DOWN
-	var directionY := Input.get_axis("ui_up", "ui_down")
-	if directionY:
-		velocity.y = directionY * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	get_input()
 	move_and_slide()
