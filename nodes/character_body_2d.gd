c4e59ee4b10fbb8734dfb3a208872860e395e290
TugSharp <smailbegovic.faris@gmail.com>
2024-10-20 20:28:26 +0200

    Camera fix, some renaming

--- nodes/character_body_2d.gd
+++ nodes/character_body_2d.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @export var speed = 50
+@onready var sprite_2d = $CharacterBody2D
+@export var sprite_direction = 0
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
