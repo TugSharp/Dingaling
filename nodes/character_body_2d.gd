081f8cd45342dc41269df7e5184460b3ade76300
TugSharp <smailbegovic.faris@gmail.com>
2024-10-28 12:20:16 +0100

    Movement Debug Test

--- nodes/character_body_2d.gd
+++ nodes/character_body_2d.gd
@@ -3,11 +3,13 @@ extends CharacterBody2D
 @export var speed = 50
 @onready var sprite_2d = $CharacterBody2D
 @export var sprite_direction = 0
+@export var input_direction = Vector4(0,0,0,0)
 
 func get_input():
-	var input_direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	input_direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity = input_direction * speed
 
 func _physics_process(delta: float) -> void:
 	get_input()
 	move_and_slide()
+	print(input_direction)
